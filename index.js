24a34c65cdb2b45f393d342189d53d3bf400be55
titouan <titouan.creach@eegle.io>
2017-11-27 14:29:58 +0100

    Pass down the $slots and $scopedSlots

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+/**
+ * The Vue documentation says that the only way to pass a scope down a child component
+ * is to use the property "scopedSlot" in the render function.
+ * In the High Order component, we receive:
+ *   - $slots That is an object where the keys are the name and the value are a VNode | Array<VNode>
+ *   - $scopedSlots That is an object where the keys are the name and the value are a function that returns
+ *     a VNode | Array<VNode>
+ *  The solution to pass slot down to the child component is to convert $slots into $scopedSlots
+ *  Here we simply bind an object like:
+ *  {
+ *    default: [VNode]
+ *  }
+ *  to
+ *  {
+ *    default: () => [VNode]
+ *  }
+ */
+function convertSlotsToScopedSlots(slots) {
+  return Object.assign({},
+    ...Object.keys(slots).map(k => ({ [k]: () => slots[k] })));
+}
 
 export function connect(store) {
   return (mapStateToProps, mapDispatchToProps) => children => ({
@@ -29,6 +50,10 @@ export function connect(store) {
             ...this.actions,
             ...this.state,
             ...this.$attrs,
+          },
+          scopedSlots: {
+            ...this.$scopedSlots,
+            ...convertSlotsToScopedSlots(this.$slots)
           },
         },
       );
