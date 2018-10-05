09c8eca0b69b861ac46386ae7ebdbdb65647ae81
titouancreach <titouan.creach@gmail.com>
2018-10-05 18:32:05 +0200

    Run npm format

--- examples/counter/src/Actions/types.js
+++ examples/counter/src/Actions/types.js
@@ -1,3 +1,3 @@
-export const INCREMENT = "INCREMENT";
-export const DECREMENT = "DECREMENT";
-export const RESET = "RESET";
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+export const RESET = 'RESET'
